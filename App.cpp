adc6532178fb3689301bb1bd9b2471b4a036c638
DESKTOP-5DLT18T\Rob Jellinghaus <rjellinghaus@live.com>
2018-05-28 15:35:29 -0700

    Refactored Tone code out into Tone class. Not tested yet but it compiles.

--- App.cpp
+++ App.cpp
@@ -38,38 +38,171 @@ void Check(bool condition)
     }
 }
 
+Tone::Tone(const App* app)    
+    : _app{ app },
+    _isSineWaveFrequencyDescending{ false },
+    _isSineWaveFrequencyChanging{ true },
+    _sineWaveFrequency{ (double)MinimumFrequencyHz },
+    _sineWavePhase{ 0 }
+{
+    _stackPanel = StackPanel{};
+
+    _stackPanel.Orientation(Orientation::Horizontal);
+    _preTextBlock = TextBlock{};
+    _preTextBlock.Text(L"Audio frame size: ");
+    _stackPanel.Children().Append(_preTextBlock);
+    _minimumAudioFrameSize = TextBlock{};
+    _stackPanel.Children().Append(_minimumAudioFrameSize);
+    _audioFrameSizeSlider = Slider{};
+    _audioFrameSizeSlider.MinWidth(300);
+    _stackPanel.Children().Append(_audioFrameSizeSlider);
+    _maximumAudioFrameSize = TextBlock{};
+    _stackPanel.Children().Append(_maximumAudioFrameSize);
+    _checkBoxLabel = TextBlock{};
+    _stackPanel.Children().Append(_checkBoxLabel);
+    _checkBox = CheckBox();
+    _stackPanel.Children().Append(_checkBox);
+    _postTextBlock = TextBlock{};
+    _stackPanel.Children().Append(_postTextBlock);
+
+    std::wstringstream wstr{};
+    wstr << _app->SamplesPerQuantum();
+    _minimumAudioFrameSize.Text(wstr.str());
+    _audioFrameSizeSlider.Minimum(_app->SamplesPerQuantum());
+    _audioInputFrameLengthInSamples = _app->SamplesPerQuantum();
+    _audioFrameSizeSlider.Maximum(_app->SampleRateHz() * 2);
+    _audioFrameSizeSlider.Value(_app->SamplesPerQuantum());
+    wstr = std::wstringstream{};
+    wstr << _audioFrameSizeSlider.Maximum();
+    _maximumAudioFrameSize.Text(wstr.str());
+    _checkBoxLabel.Text(L"Change frequency? ");
+
+    // Create the frame input node AFTER the graph has started.
+    // (This is the scenario used by the app I'm writing, which has to work free of crackling/static issues in playback.)
+    _audioFrameInputNode = _app->Graph().CreateFrameInputNode();
+    _audioFrameInputNode.QuantumStarted([&](AudioFrameInputNode sender, FrameInputNodeQuantumStartedEventArgs args)
+    {
+        FrameInputNode_QuantumStarted(sender, args);
+    });
+    _audioFrameInputNode.AddOutgoingConnection(_app->OutputNode());
+
+    Check(_audioFrameInputNode.EncodingProperties().SampleRate() == _app->SampleRateHz());
+    Check(_audioFrameInputNode.EncodingProperties().ChannelCount() == _app->ChannelCount());
+}
+
+void Tone::UpdateUI()
+{
+    // Update audio input frame length from slider.  (Value() method evidently is not agile.)
+    _audioInputFrameLengthInSamples = (uint32_t)_audioFrameSizeSlider.Value();
+    _isSineWaveFrequencyChanging = _checkBox.IsChecked().Value();
+
+    std::wstringstream wstr{};
+    wstr << "Input frame length: " << _audioInputFrameLengthInSamples
+        << " | Input frame samples: " << _audioInputFrameSampleCount
+        << " | Zero frame count: " << _zeroByteOutgoingFrameCount
+        << " | Sine wave frequency: " << _sineWaveFrequency;
+    _postTextBlock.Text(wstr.str());
+}
+
+void Tone::FrameInputNode_QuantumStarted(AudioFrameInputNode sender, FrameInputNodeQuantumStartedEventArgs args)
+{
+    Check(sender == _audioFrameInputNode);
+
+    Check(args.RequiredSamples() >= 0);
+    uint32_t requiredSamples = (uint32_t)args.RequiredSamples();
+
+    if (requiredSamples == 0)
+    {
+        _zeroByteOutgoingFrameCount++;
+        return;
+    }
+
+    // kenneth, what is the frequency
+    const double sineWaveFrequencyChange = ((double)_app->SamplesPerQuantum() / _app->SampleRateHz())
+        / FrequencyCycleTimeSecs
+        * (MaximumFrequencyHz - MinimumFrequencyHz);
+
+    if (_isSineWaveFrequencyChanging)
+    {
+        if (_isSineWaveFrequencyDescending)
+        {
+            _sineWaveFrequency -= sineWaveFrequencyChange;
+            if (_sineWaveFrequency < MinimumFrequencyHz)
+            {
+                _isSineWaveFrequencyDescending = false;
+            }
+        }
+        else
+        {
+            _sineWaveFrequency += sineWaveFrequencyChange;
+            if (_sineWaveFrequency > MaximumFrequencyHz)
+            {
+                _isSineWaveFrequencyDescending = true;
+            }
+        }
+    }
+
+    // we are looping; let's play!
+    // float samplesSinceLastQuantum = (float)sinceLast.count() * _sampleRateHz / TicksPerSecond;
+    // _requiredSamplesHistogram.Add(requiredSamples);
+    // _sinceLastSampleTimingHistogram.Add(samplesSinceLastQuantum);
+
+    uint32_t frameSizeInSamples = _audioInputFrameLengthInSamples;
+    _audioInputFrameSampleCount += frameSizeInSamples;
+
+    // Stereo float samples outbound in this audio frame.
+    Windows::Media::AudioFrame audioFrame(frameSizeInSamples * sizeof(float) * _app->ChannelCount());
+
+    {
+        // This nested scope sets the extent of the LockBuffer call below, which must close before the AddFrame call.
+        // Otherwise the AddFrame will throw E_ACCESSDENIED when it tries to take a read lock on the frame.
+        uint8_t* dataInBytes{};
+        uint32_t capacityInBytes{};
+
+        // OMG KENNY KERR WINS AGAIN:
+        // https://gist.github.com/kennykerr/f1d941c2d26227abbf762481bcbd84d3
+        Windows::Media::AudioBuffer buffer(audioFrame.LockBuffer(Windows::Media::AudioBufferAccessMode::Write));
+        IMemoryBufferReference reference(buffer.CreateReference());
+        winrt::impl::com_ref<IMemoryBufferByteAccess> interop = reference.as<IMemoryBufferByteAccess>();
+        check_hresult(interop->GetBuffer(&dataInBytes, &capacityInBytes));
+
+        Check((capacityInBytes % _app->BytesPerSample()) == 0);
+        Check(capacityInBytes / _app->BytesPerSample() == frameSizeInSamples);
+
+        float* data = (float*)dataInBytes;
+        const double amplitude = 0.8;
+        const double pi = std::acos(-1);
+        // The current frequency only affects the change in phase; it does not actually alter the phase
+        // at any point. So there should never be any audible clicks or pops as a result of a frequency
+        // change here.
+        const double phaseIncrement = _sineWaveFrequency * 2 * pi / _app->SampleRateHz();
+        for (uint32_t i = 0; i < frameSizeInSamples; i++)
+        {
+            // set both stereo channels to same sine value
+            data[i * 2] = data[i * 2 + 1] = (float)sin(_sineWavePhase);
+            _sineWavePhase += phaseIncrement;
+            if (_sineWavePhase > 2 * pi)
+            {
+                _sineWavePhase -= 2 * pi;
+            }
+        }
+    }
+
+    sender.AddFrame(audioFrame);
+}
+
 void App::OnLaunched(LaunchActivatedEventArgs const&)
 {
     _textBlockGraphStatus = TextBlock();
     _textBlockGraphStatus.Text(L"");
     _textBlockGraphInfo = TextBlock();
     _textBlockGraphInfo.Text(L"");
-    _textBlockTimeInfo = TextBlock();
-    _textBlockTimeInfo.Text(L"");
 
     Window xamlWindow = Window::Current();
 
     _stackPanel = StackPanel();
     _stackPanel.Children().Append(_textBlockGraphStatus);
     _stackPanel.Children().Append(_textBlockGraphInfo);
-    _stackPanel.Children().Append(_textBlockTimeInfo);
-
-    StackPanel sliderPanel{};
-    sliderPanel.Orientation(Orientation::Horizontal);
-    TextBlock sliderLabel{};
-    sliderLabel.Text(L"Audio frame size: ");
-    sliderPanel.Children().Append(sliderLabel);
-    _minimumAudioFrameSize = TextBlock();
-    _minimumAudioFrameSize.Text(L"_");
-    sliderPanel.Children().Append(_minimumAudioFrameSize);
-    _audioFrameSizeSlider = Slider();
-    _audioFrameSizeSlider.MinWidth(300);
-    sliderPanel.Children().Append(_audioFrameSizeSlider);
-    _maximumAudioFrameSize = TextBlock();
-    _maximumAudioFrameSize.Text(L"_");
-    sliderPanel.Children().Append(_maximumAudioFrameSize);
-
-    _stackPanel.Children().Append(sliderPanel);
 
     xamlWindow.Content(_stackPanel);
     xamlWindow.Activate();
@@ -92,47 +225,28 @@ fire_and_forget App::LaunchedAsync()
 
     // Make local variable for audio graph to ensure preserved across coroutine boundaries.
     // (If just instance variable, it would become inaccessible.)
-    AudioGraph audioGraph = result.Graph();
-    int latencyInSamples = audioGraph.LatencyInSamples();
-    _samplesPerQuantum = audioGraph.SamplesPerQuantum();
-    _sampleRateHz = audioGraph.EncodingProperties().SampleRate();
-    _channelCount = audioGraph.EncodingProperties().ChannelCount();
+    _audioGraph = result.Graph();
+    _latencyInSamples = _audioGraph.LatencyInSamples();
+    _samplesPerQuantum = _audioGraph.SamplesPerQuantum();
+    _sampleRateHz = _audioGraph.EncodingProperties().SampleRate();
+    _channelCount = _audioGraph.EncodingProperties().ChannelCount();
 
     _audioGraphQuantumCount = 0;
     _audioGraphQuantumSampleCount = 0;
 
     // make sure we're float encoding
-    Check(audioGraph.EncodingProperties().BitsPerSample() == sizeof(float) * 8);
+    Check(_audioGraph.EncodingProperties().BitsPerSample() == sizeof(float) * 8);
     // make sure we're stereo output (only mode supported for this tiny demo)
     Check(_channelCount == 2);
 
-    _audioGraph = audioGraph;
     _audioGraphQuantumCount = 0;
 
     _bytesPerSample = sizeof(float) * _channelCount;
 
-    // switch to UI thread to update UI, now that we know the audio graph's state
-    co_await _uiThread;
-    std::wstringstream wstr;
-    wstr << L"Sample rate: " << _sampleRateHz
-        << L"| Latency in samples: " << latencyInSamples
-        << " | Samples per quantum: " << _samplesPerQuantum;
-    _textBlockGraphInfo.Text(wstr.str());
-
-    wstr = std::wstringstream{};
-    wstr << _samplesPerQuantum;
-    _minimumAudioFrameSize.Text(wstr.str());
-    _audioFrameSizeSlider.Minimum(_samplesPerQuantum);
-    _audioInputFrameLengthInSamples = _samplesPerQuantum;
-    _audioFrameSizeSlider.Maximum(_sampleRateHz * 2);
-    _audioFrameSizeSlider.Value(_samplesPerQuantum);
-    wstr = std::wstringstream{};
-    wstr << _audioFrameSizeSlider.Maximum();
-    _maximumAudioFrameSize.Text(wstr.str());
     co_await resume_background();
 
     // Create a device output node
-    CreateAudioDeviceOutputNodeResult deviceOutputNodeResult = co_await audioGraph.CreateDeviceOutputNodeAsync();
+    CreateAudioDeviceOutputNodeResult deviceOutputNodeResult = co_await _audioGraph.CreateDeviceOutputNodeAsync();
 
     Check(deviceOutputNodeResult.Status() == AudioDeviceNodeCreationStatus::Success);
 
@@ -143,44 +257,17 @@ fire_and_forget App::LaunchedAsync()
         _audioGraphQuantumSampleCount += _samplesPerQuantum;
         _audioGraphQuantumCount++;
 
-        // kenneth, what is the frequency
-        const double sineWaveFrequencyChange = ((float)_samplesPerQuantum / _sampleRateHz) / FrequencyCycleTimeSecs * (MaximumFrequencyHz - MinimumFrequencyHz);
-        if (_isSineWaveFrequencyDescending)
-        {
-            _sineWaveFrequency -= sineWaveFrequencyChange;
-            if (_sineWaveFrequency < MinimumFrequencyHz)
-            {
-                _isSineWaveFrequencyDescending = false;
-            }
-        }
-        else
-        {
-            _sineWaveFrequency += sineWaveFrequencyChange;
-            if (_sineWaveFrequency > MaximumFrequencyHz)
-            {
-                _isSineWaveFrequencyDescending = true;
-            }
-        }
     });
 
-    audioGraph.Start();
+    _audioGraph.Start();
 
     // This must be called on the UI thread.
     co_await _uiThread;
     _textBlockGraphStatus.Text(L"Graph started");
+
+    _tones.push_back(std::unique_ptr<Tone>(new Tone(this)));
+
     co_await resume_background();
-
-    // Create the frame input node AFTER the graph has started.
-    // (This is the scenario used by the app I'm writing, which has to work free of crackling/static issues in playback.)
-    _audioFrameInputNode = audioGraph.CreateFrameInputNode();
-    _audioFrameInputNode.QuantumStarted([&](AudioFrameInputNode sender, FrameInputNodeQuantumStartedEventArgs args)
-    {
-        FrameInputNode_QuantumStarted(sender, args);
-    });
-    _audioFrameInputNode.AddOutgoingConnection(deviceOutputNode);
-
-    Check(_audioFrameInputNode.EncodingProperties().SampleRate() == _sampleRateHz);
-    Check(_audioFrameInputNode.EncodingProperties().ChannelCount() == _channelCount);
 
     UpdateLoop();
 }
@@ -197,86 +284,22 @@ fire_and_forget App::UpdateLoop()
         
         co_await _uiThread;
 
-        // Update audio input frame length from slider.  (Value() method evidently is not agile.)
-        _audioInputFrameLengthInSamples = (uint32_t)_audioFrameSizeSlider.Value();
+        // switch to UI thread to update UI, now that we know the audio graph's state
+        std::wstringstream wstr;
+        wstr << L"Sample rate: " << _sampleRateHz
+            << L"| Latency in samples: " << _latencyInSamples
+            << " | Samples per quantum: " << _samplesPerQuantum
+            << " | Graph quantum count: " << _audioGraphQuantumCount
+            << " | Graph sample count: " << _audioGraphQuantumSampleCount;
+        _textBlockGraphInfo.Text(wstr.str());
 
-        std::wstringstream wstr{};
-        wstr << "Input frame length: " << _audioInputFrameLengthInSamples
-            << " | Audio graph quanta: " << _audioGraphQuantumCount
-            << " | Audio graph samples: " << _audioGraphQuantumSampleCount
-            << " | Input frame samples: " << _audioInputFrameSampleCount
-            << " | Zero frame count: " << _zeroByteOutgoingFrameCount
-            << " | Sine wave frequency: " << _sineWaveFrequency;
-        _textBlockTimeInfo.Text(wstr.str());
+        for (std::unique_ptr<Tone>& tone : _tones)
+        {
+            tone.get()->UpdateUI();
+        }
 
         co_await resume_background();
     }
-}
-
-void App::FrameInputNode_QuantumStarted(AudioFrameInputNode sender, FrameInputNodeQuantumStartedEventArgs args)
-{
-    Check(sender == _audioFrameInputNode);
-
-    Check(args.RequiredSamples() >= 0);
-    uint32_t requiredSamples = (uint32_t)args.RequiredSamples();
-
-    if (requiredSamples == 0)
-    {
-        _zeroByteOutgoingFrameCount++;
-        return;
-    }
-
-    DateTime dateTimeNow = DateTime::clock::now();
-    TimeSpan sinceLast = dateTimeNow - _lastQuantumTime;
-    _lastQuantumTime = dateTimeNow;
-
-    // we are looping; let's play!
-    // float samplesSinceLastQuantum = (float)sinceLast.count() * _sampleRateHz / TicksPerSecond;
-    // _requiredSamplesHistogram.Add(requiredSamples);
-    // _sinceLastSampleTimingHistogram.Add(samplesSinceLastQuantum);
-
-    uint32_t frameSizeInSamples = _audioInputFrameLengthInSamples;
-    _audioInputFrameSampleCount += frameSizeInSamples;
-
-    // Stereo float samples outbound in this audio frame.
-    Windows::Media::AudioFrame audioFrame(frameSizeInSamples * sizeof(float) * _channelCount);
-
-    {
-        // This nested scope sets the extent of the LockBuffer call below, which must close before the AddFrame call.
-        // Otherwise the AddFrame will throw E_ACCESSDENIED when it tries to take a read lock on the frame.
-        uint8_t* dataInBytes{};
-        uint32_t capacityInBytes{};
-
-        // OMG KENNY KERR WINS AGAIN:
-        // https://gist.github.com/kennykerr/f1d941c2d26227abbf762481bcbd84d3
-        Windows::Media::AudioBuffer buffer(audioFrame.LockBuffer(Windows::Media::AudioBufferAccessMode::Write));
-        IMemoryBufferReference reference(buffer.CreateReference());
-        winrt::impl::com_ref<IMemoryBufferByteAccess> interop = reference.as<IMemoryBufferByteAccess>();
-        check_hresult(interop->GetBuffer(&dataInBytes, &capacityInBytes));
-
-        Check((capacityInBytes % _bytesPerSample) == 0);
-        Check(capacityInBytes / _bytesPerSample == frameSizeInSamples);
-
-        float* data = (float*)dataInBytes;
-        const double amplitude = 0.8;
-        const double pi = std::acos(-1);
-        // The current frequency only affects the change in phase; it does not actually alter the phase
-        // at any point. So there should never be any audible clicks or pops as a result of a frequency
-        // change here.
-        const double phaseIncrement = _sineWaveFrequency * 2 * pi / _sampleRateHz;
-        for (uint32_t i = 0; i < frameSizeInSamples; i++)
-        {
-            // set both stereo channels to same sine value
-            data[i * 2] = data[i * 2 + 1] = (float)sin(_sineWavePhase);
-            _sineWavePhase += phaseIncrement;
-            if (_sineWavePhase > 2 * pi)
-            {
-                _sineWavePhase -= 2 * pi;
-            }
-        }
-    }
-
-    sender.AddFrame(audioFrame);
 }
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
--- App.h
+++ App.h
@@ -19,12 +19,84 @@ winrt::Windows::Foundation::TimeSpan timeSpanFromSeconds(int seconds);
 // release assertion, basically
 void Check(bool condition);
 
+class App;
+
+// A given Tone represents a single sine wave input to the audio graph, along with the
+// UI controls that manipulate the Tone.
+class Tone
+{
+public: // constants
+        // Minimum frequency (and starting frequency as it happens).
+    const uint32_t MinimumFrequencyHz = 300;
+    // Maximum frequency.
+    const uint32_t MaximumFrequencyHz = 900;
+    // Time to cycle from min to max. Also time to cycle from max back to min.
+    const uint32_t FrequencyCycleTimeSecs = 4;
+
+private:
+    winrt::Windows::UI::Xaml::Controls::StackPanel _stackPanel{ nullptr };
+
+    winrt::Windows::UI::Xaml::Controls::TextBlock _preTextBlock{ nullptr };
+
+    winrt::Windows::UI::Xaml::Controls::TextBlock _minimumAudioFrameSize{ nullptr };
+    winrt::Windows::UI::Xaml::Controls::Slider _audioFrameSizeSlider{ nullptr };
+    winrt::Windows::UI::Xaml::Controls::TextBlock _maximumAudioFrameSize{ nullptr };
+
+    winrt::Windows::UI::Xaml::Controls::TextBlock _checkBoxLabel{ nullptr };
+    winrt::Windows::UI::Xaml::Controls::CheckBox _checkBox{ nullptr };
+
+    winrt::Windows::UI::Xaml::Controls::TextBlock _postTextBlock{ nullptr };
+
+    // The App that instantiated this.
+    const App* _app;
+
+private: // sound/frequency state
+    // Current frequency of sine wave -- updated by audio graph quantum.
+    double _sineWaveFrequency;
+
+    // Current phase of sine wave; ranges from 0 to 2*pi.
+    double _sineWavePhase;
+
+    // Is sine wave frequency descending?  (If not, it's ascending.)
+    bool _isSineWaveFrequencyDescending;
+
+    // Total sample count measured by required samples for audio frame input node.
+    uint32_t _audioInputFrameSampleCount;
+
+    // The audio input frame length (in samples), set from the slider.
+    uint32_t _audioInputFrameLengthInSamples;
+
+    // It is not clear why FrameInputNode_QuantumStarted is called with zero required bytes.
+    // But it evidently is, as this variable indicates.
+    int _zeroByteOutgoingFrameCount;
+
+    winrt::Windows::Media::Audio::AudioFrameInputNode _audioFrameInputNode{ nullptr };
+
+    // user can set whether frequency is changing or not
+    bool _isSineWaveFrequencyChanging;
+
+public: // properties
+    winrt::Windows::UI::Xaml::Controls::StackPanel Panel() const { return _stackPanel; }
+
+public:
+    // Tones must be constructed from the UI context.
+    Tone(const App* app);
+
+    // Must be called from the ui context.
+    void UpdateUI();
+
+    // The quantum has started; consume input audio for this recording.
+    void FrameInputNode_QuantumStarted(
+        winrt::Windows::Media::Audio::AudioFrameInputNode sender,
+        winrt::Windows::Media::Audio::FrameInputNodeQuantumStartedEventArgs args);
+};
+
 // Simple application which generates a sine wave at a fixed frequency, while changing the audio frame size.
 // Basically equivalent to the sample code at https://docs.microsoft.com/en-us/windows/uwp/audio-video-camera/audio-graphs#audio-frame-input-node
 // except that it allows interactively varying the audio input frame size.
 // This should not affect the sound.  On some platforms, it evidently does (as of Win 10 version 1803).
 // Hopefully this app will help to reproduce the issue on such platforms so it can be fixed.
-struct App : winrt::Windows::UI::Xaml::ApplicationT<App>
+class App : public winrt::Windows::UI::Xaml::ApplicationT<App>
 {
     // The interaction model of this app is:
     // - App displays window with slider.
@@ -39,42 +111,24 @@ struct App : winrt::Windows::UI::Xaml::ApplicationT<App>
     // cause clicking or popping.  I encourage reviewers to validate this claim; it's the entire point of this sample,
     // to root-cause the audible clicking/popping issues with shorter frame sizes.
 
-public: // constants
-    // Minimum frequency (and starting frequency as it happens).
-    const uint32_t MinimumFrequencyHz = 300;
-    // Maximum frequency.
-    const uint32_t MaximumFrequencyHz = 900;
-    // Time to cycle from min to max. Also time to cycle from max back to min.
-    const uint32_t FrequencyCycleTimeSecs = 4;
-
-public: // UI controls
+private: // UI controls
     winrt::Windows::UI::Xaml::Controls::TextBlock _textBlockGraphStatus{ nullptr };
     winrt::Windows::UI::Xaml::Controls::TextBlock _textBlockGraphInfo{ nullptr };
-    winrt::Windows::UI::Xaml::Controls::TextBlock _textBlockTimeInfo{ nullptr };
-
-    winrt::Windows::UI::Xaml::Controls::TextBlock _minimumAudioFrameSize{ nullptr };
-    winrt::Windows::UI::Xaml::Controls::Slider _audioFrameSizeSlider{ nullptr };
-    winrt::Windows::UI::Xaml::Controls::TextBlock _maximumAudioFrameSize{ nullptr };
 
     winrt::Windows::UI::Xaml::Controls::StackPanel _stackPanel{ nullptr };
 
-public: // threading
+    std::vector<std::unique_ptr<Tone>> _tones{};
+
+private: // threading
     // The apartment context of the UI thread; must co_await this before updating UI
     // (and must thereafter switch out of UI context ASAP, for liveness).
     winrt::apartment_context _uiThread;
 
-public: // sound generation
-    // Current frequency of sine wave -- updated by audio graph quantum.
-    double _sineWaveFrequency;
-
-    // Current phase of sine wave; ranges from 0 to 2*pi.
-    double _sineWavePhase;
-
-    // Is sine wave frequency descending?  (If not, it's ascending.)
-    bool _isSineWaveFrequencyDescending;
-
+private: // sound generation
     // sample rate of audio graph
     uint32_t _sampleRateHz;
+    // latency in samples reported by audio graph
+    uint32_t _latencyInSamples;
     // channel count of audio graph
     uint32_t _channelCount;
     // samples per quantum of audio graph
@@ -86,28 +140,20 @@ public: // sound generation
     // The number of QuantumStarted events we've received from the audio graph.
     uint32_t _audioGraphQuantumCount;
 
-    // Total sample count measured by required samples for audio frame input node.
-    uint32_t _audioInputFrameSampleCount;
-
-    // The audio input frame length (in samples), set from the slider.
-    uint32_t _audioInputFrameLengthInSamples;
-
     // bytes per sample (channel count * sample size in bytes)
     uint32_t _bytesPerSample;
 
     winrt::Windows::Media::Audio::AudioGraph _audioGraph{ nullptr };
-    winrt::Windows::Media::Audio::AudioFrameInputNode _audioFrameInputNode{ nullptr };
+    winrt::Windows::Media::Audio::AudioDeviceOutputNode _audioDeviceOutputNode{ nullptr };
 
-    winrt::Windows::Foundation::DateTime _lastQuantumTime;
+public: // properties
+    winrt::Windows::Media::Audio::AudioGraph Graph() const { return _audioGraph; }
+    winrt::Windows::Media::Audio::AudioDeviceOutputNode OutputNode() const { return _audioDeviceOutputNode; }
 
-    // The quantum has started; consume input audio for this recording.
-    void FrameInputNode_QuantumStarted(
-        winrt::Windows::Media::Audio::AudioFrameInputNode sender,
-        winrt::Windows::Media::Audio::FrameInputNodeQuantumStartedEventArgs args);
-
-    // It is not clear why FrameInputNode_QuantumStarted is called with zero required bytes.
-    // But it evidently is, as this variable indicates.
-    int _zeroByteOutgoingFrameCount;
+    uint32_t SampleRateHz() const { return _sampleRateHz; }
+    uint32_t ChannelCount() const { return _channelCount; }
+    uint32_t SamplesPerQuantum() const { return _samplesPerQuantum; }
+    uint32_t BytesPerSample() const { return _bytesPerSample; }
 
 public: // application implementation
     winrt::fire_and_forget LaunchedAsync();
